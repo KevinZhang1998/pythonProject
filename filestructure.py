32393aeef026277f88ecfe923e785fdf242df6e5
KevinZhang1998 <1633183459@qq.com>
2019-09-02 10:09:09 +0800

    Update filestructure.py

--- filestructure.py
+++ filestructure.py
@@ -5,14 +5,14 @@ from openpyxl import load_workbook
 def get_file_structure(path):
     for root, dics, files in os.walk(path):
         for file in files:
-            file_path =(os.path.join(root.replace("E:\\张文杰\\",""), file)).split('\\')
+            file_path =(os.path.join(root.replace("E:\\xxx\\",""), file)).split('\\')
             yield file_path
 
 
 if __name__ == '__main__':
     wb = load_workbook('structure.xlsx')
     ws = wb.active
-    for i in get_file_structure('E:\张文杰\燎原学院'):
+    for i in get_file_structure('E:\xxx\xxx'):
         ws.append(i)
     wb.save('structure.xlsx')
 
